52badf0c6d6794054edf85f3cee3e34aa588f1e1
VitalikS228 <119817339+VitalikS228@users.noreply.github.com>
2023-12-20 14:41:17 +0300

    adding DZ2
    task: 3.19; 4.1b

---
+++ t4_1b.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class t4_1b {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть значення x: ");
+        double x = scanner.nextDouble();
+
+        double result = calculatePolynomial(x);
+
+        System.out.println("Результат обчислення многочлена: " + result);
+    }
+
+    public static double calculatePolynomial(double x) {
+        // Обчислення значення многочлена y = x^4 + x^3 + x^2 + x + 1
+        double xSquared = x * x;
+        double xCubed = xSquared * x;
+        double xToPower4 = xCubed * x;
+        double result = xToPower4 + xCubed + xSquared + x + 1;
+
+        return result;
+    }
+}
